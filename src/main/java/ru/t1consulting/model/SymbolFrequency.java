38b56c1c2180c550f8511e5ed484b2ba1d7e1231
Lovtzoff <lovtzoff.aleksei@gmail.com>
2023-11-01 13:34:01 +0300

    Add README, service and test

--- src/main/java/ru/t1consulting/model/SymbolFrequency.java
+++ src/main/java/ru/t1consulting/model/SymbolFrequency.java
@@ -1,10 +1,12 @@
 package ru.t1consulting.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class SymbolFrequency {
 
     private char symbol;
---
+++ src/main/java/ru/t1consulting/service/SymbolFrequencyService.java
@@ -0,0 +1,30 @@
+package ru.t1consulting.service;
+
+import org.springframework.stereotype.Service;
+import ru.t1consulting.model.SymbolFrequency;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class SymbolFrequencyService {
+
+    public List<SymbolFrequency> getSymbolFrequency(String inputString) {
+        // вычисляем частоту встречи символов в строке
+        Map<Character, Integer> frequencyMap = inputString.chars()
+                // преобразуем int в char
+                .mapToObj(c -> (char) c)
+                // группируем по символам и суммируем количество
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
+
+        return frequencyMap.entrySet().stream()
+                // преобразуем Map.Entry в SymbolFrequency
+                .map(entry -> new SymbolFrequency(entry.getKey(), entry.getValue()))
+                // сортируем по частоте в обратном порядке
+                .sorted(Comparator.comparingInt(SymbolFrequency::getFrequency).reversed())
+                .collect(Collectors.toList());
+    }
+}
